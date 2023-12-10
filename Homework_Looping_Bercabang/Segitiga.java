897b2cf9096164c2279bdee3b6aeca3d77941a06
Dinda Yustika <84846008+Gvskaa@users.noreply.github.com>
2023-12-10 21:32:40 +0700

    Add files via upload

---
+++ Homework_Looping_Bercabang/Segitiga.java
@@ -0,0 +1,18 @@
+package Homework_Looping_Bercabang;
+
+public class Segitiga {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.print("* ");
+            for (int j = 0; j< 9; j++){
+                System.out.print("* ");
+                if(i == j){
+                    break;
+                } else if ((i + j) == 9) {
+                    break;
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+}
